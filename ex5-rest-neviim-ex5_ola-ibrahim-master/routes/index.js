ed585aaa35eedbaa6cf494fa7c8a1f8afebfec84
Ola Ibrahim <92628525+olaib@users.noreply.github.com>
2023-04-02 12:19:47 +0300

    Delete ex5-rest-neviim-ex5_ola-ibrahim-master directory

--- ex5-rest-neviim-ex5_ola-ibrahim-master/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const PAGE_NOT_FOUND = 404;
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ex-5 Nasa Api' });
-});
-module.exports = router;
